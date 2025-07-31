34dc686c27c0d89f7a71ffa99a6a79dc73992341
Aman kumar <140549982+amansinghh07@users.noreply.github.com>
2025-07-31 17:27:36 +0530

    Initial commit

---
+++ Bank/src/main/java/com/security/bank/entity/Account.java
@@ -0,0 +1,34 @@
+package com.security.bank.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private AccountType accountType;
+    private String status;
+    private double balance;
+    private float interestRate;
+    @Enumerated(EnumType.STRING)
+    private BranchType branch;
+    private String proof;
+    private Date openingDate;
+    private Long accountNumber;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Nominee nominee;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Card card;
+
+    @ManyToOne
+    @JsonIgnoreProperties("accountList")
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
